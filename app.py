ed778f499a6a185deba9949b86088d74b669740f
Kajal Kumari <156607406+Kaju223@users.noreply.github.com>
2026-07-12 22:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-
+import os
 import streamlit as st
 import google.generativeai as genai
 
 # Configure Gemini API
-genai.configure(api_key="GEMINI_API_KEY ")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY")
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
